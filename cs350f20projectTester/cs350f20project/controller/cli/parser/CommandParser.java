dc03e36ee1775d2910f07eef0db1fd19343d1b01
Keyes-bits <65564958+Keyes-bits@users.noreply.github.com>
2020-11-18 10:09:46 -0800

    Add files via upload
    This is the CommandParser that currently works - basic info, will need to be updated
    - There is an additional Tester class that can be used to test the individual commands

---
+++ cs350f20projectTester/cs350f20project/controller/cli/parser/CommandParser.java
@@ -0,0 +1,27 @@
+package cs350f20project.controller.cli.parser;
+
+import cs350f20project.controller.command.A_Command;
+import cs350f20project.controller.command.meta.CommandMetaDoExit;
+
+public class CommandParser {
+	
+	private MyParserHelper parserHelper;
+	private String commandText;
+
+	public CommandParser(MyParserHelper parserHelper, String commandText)
+	{
+		this.parserHelper = parserHelper;
+		this.commandText = commandText;
+	}
+	
+	public void parse()
+	{
+		if (this.commandText.equalsIgnoreCase("@exit"))
+		{
+			A_Command command = new CommandMetaDoExit();
+			this.parserHelper.getActionProcessor().schedule(command);
+		} 
+	}
+	
+}
+
---
+++ cs350f20projectTester/cs350f20project/controller/cli/parser/Tester.java
@@ -0,0 +1,14 @@
+package cs350f20project.controller.cli.parser;
+
+import cs350f20project.Startup;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		
+		 Startup whatever = new Startup();
+		 
+		 whatever.go();
+	}
+
+}
