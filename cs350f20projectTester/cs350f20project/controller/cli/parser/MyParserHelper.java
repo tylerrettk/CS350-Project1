e4e0004f470366af33aadb49e5c6a6c2aaf88f78
tylerrettk <61598953+tylerrettk@users.noreply.github.com>
2020-11-18 20:34:52 -0800

    myparserhelper
    d

---
+++ cs350f20projectTester/cs350f20project/controller/cli/parser/MyParserHelper.java
@@ -0,0 +1,12 @@
+package cs350f20project.controller.cli.parser;
+
+import cs350f20project.controller.ActionProcessor;
+
+public class MyParserHelper extends A_ParserHelper {
+	
+	public MyParserHelper(ActionProcessor actionProcessor)
+	{
+		super(actionProcessor);
+	}
+
+}
