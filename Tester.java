68b4a2762e6cb3759b0712f5d9d638f0d896347f
Mohammed518 <57207499+Mohammed518@users.noreply.github.com>
2020-12-02 19:13:07 -0800

    Create Tester.java

---
+++ Tester.java
@@ -0,0 +1,12 @@
+package cs350f20project.test;
+
+import cs350f20project.Startup;
+
+public class Tester {
+
+  public static void main(String args[]) {
+    Startup test = new Startup();
+    test.go();
+  }
+
+}
