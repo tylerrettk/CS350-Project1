8393827b085259226b61d6fa7ced96746bb66d8e
Mohammed518 <57207499+Mohammed518@users.noreply.github.com>
2020-12-02 19:11:19 -0800

    Create StructuralParser.java

---
+++ StructuralParser.java
@@ -0,0 +1,32 @@
+package cs350f20project.controller.cli.parser;
+
+import cs350f20project.datatype.*;
+import cs350f20project.support.*;
+import cs350f20project.controller.*;
+import cs350f20project.controller.cli.*;
+import cs350f20project.controller.command.*;
+import cs350f20project.controller.command.creational.*;
+import cs350f20project.controller.command.structural.*;
+import cs350f20project.controller.command.behavioral.*;
+import cs350f20project.controller.command.meta.*;
+import cs350f20project.controller.timing.*;
+
+import java.io.*;
+import java.util.*;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import cs350f20project.controller.cli.parser.HelperMethods;
+
+public class StructuralParser {
+  private A_ParserHelper parserHelper;
+
+  public StructuralParser(MyParserHelper parserHelper) {
+    this.parserHelper = parserHelper;
+  }
+
+  public void parseStructCmds(String cmd) {
+    ;
+  }
+}
